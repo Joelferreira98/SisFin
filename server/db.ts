00cdae1d70ca48a2ddc10f9e77c8cbe8b6f13b58
Joelferreira98 <124409388+Joelferreira98@users.noreply.github.com>
2025-07-16 08:41:11 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,13 +1,10 @@
 import dotenv from "dotenv";
-// Carregar variáveis de ambiente ANTES de qualquer importação
 dotenv.config();
 
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
-import * as schema from "@shared/schema";
+import { Pool } from 'pg'
+import { drizzle } from 'drizzle-orm/node-postgres';
 
-neonConfig.webSocketConstructor = ws;
+import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
   console.error("❌ DATABASE_URL não encontrada!");
@@ -19,4 +16,5 @@ if (!process.env.DATABASE_URL) {
 }
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+export const db = drizzle(pool, { schema });
+
